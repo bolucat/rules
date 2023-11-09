66e6decc67a6eee7f37c23282be518d0ad0211db
Pocat <hglkr@pm.me>
2023-11-09 18:48:49 +0800

    add AdGurad-Base-Without-Easylist

--- run.sh
+++ run.sh
@@ -219,6 +219,7 @@ adblock() {
 	wget https://raw.githubusercontent.com/List-KR/List-KR/master/filter-AdGuard.txt -O List-KR.txt
 	wget https://easylist-downloads.adblockplus.org/ruadlist.txt -O RU-AdList.txt
  	echo "! Title: Adguard Base Filter" > AdGurad-Base.txt && wget -qO- https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt >> AdGurad-Base.txt
+  	wget https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt -O AdGurad-Base-Without-Easylist.txt
 	wget https://filters.adtidy.org/extension/ublock/filters/224.txt -O AdGuard-Chinese.txt
 	wget https://easylist.to/easylistgermany/easylistgermany.txt -O EasyList-Germany.txt
 	wget https://filters.adtidy.org/extension/ublock/filters/7.txt -O AdGuard-Japanese.txt
