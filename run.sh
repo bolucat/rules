461f9e3ac2bd16dfd3358ad31a4244b469f9a652
Pocat <hglkr@pm.me>
2023-11-09 18:58:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -214,7 +214,7 @@ adblock() {
 	wget https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt -O uBlock-Annoyances.txt
 	wget https://malware-filter.gitlab.io/malware-filter/phishing-filter-agh.txt -O Phishing-URL-Blocklist.txt
 	wget https://malware-filter.gitlab.io/malware-filter/botnet-filter-ag.txt -O Botnet-Blocklist.txt
-	wget https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-agh.txt -O Malicious-URL-Blocklist.txt
+	wget https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-ag-online.txt -O Malicious-URL-Blocklist.txt
 	echo "! Title: Dan Pollocks hosts" > Dan-Pollocks-hosts.txt && wget -qO- https://someonewhocares.org/hosts/hosts >> Dan-Pollocks-hosts.txt
 	wget https://raw.githubusercontent.com/List-KR/List-KR/master/filter-AdGuard.txt -O List-KR.txt
 	wget https://easylist-downloads.adblockplus.org/ruadlist.txt -O RU-AdList.txt
